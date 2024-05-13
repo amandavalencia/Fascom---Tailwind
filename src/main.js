0d363dc40cf833002b1911bc07dbfc174f7963cd
amandaavalencia <amandaavalencia@hotmail.com>
2024-05-13 22:23:01 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+const toggleDarkmode = document.getElementById("toggleDarkmode");
+const darkmodeButton = document.getElementById("darkmodeButton").addEventListener("click", ()=>{
+    toggleDarkmode.classList.toggle("dark");
+})
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    screens: {
+      'sm': '640px',
+    },
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [],
+  
+}
+
